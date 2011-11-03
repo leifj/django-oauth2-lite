f2d41099413e87bb743a42607bedf1a953502649
Leif Johansson <leifj@sunet.se>
2011-11-03 17:26:36 +0100

    more forms

--- django_oauth2_lite/forms.py
+++ django_oauth2_lite/forms.py
@@ -5,7 +5,7 @@ Created on Nov 1, 2011
 '''
 
 from django.forms.models import ModelForm
-from django_oauth2_lite.models import Code
+from django_oauth2_lite.models import Code, Client
 from django.forms.forms import Form
 from django.forms.fields import CharField, URLField
     
@@ -13,6 +13,11 @@ class CodeForm(ModelForm):
     
     class Meta:
         model = Code
+        
+class ClientForm(ModelForm):
+    
+    class Meta:
+        model = Client
         
 class AccessRequest(Form):
     grant_type = CharField(required=True)
