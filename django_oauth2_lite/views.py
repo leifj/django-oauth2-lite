f0923c08c7e7b5516a7e0535a6e3ea558e1256f8
Leif Johansson <leifj@sunet.se>
2011-11-03 17:27:38 +0100

    various bugfixes and improvements

--- django_oauth2_lite/views.py
+++ django_oauth2_lite/views.py
@@ -1,8 +1,8 @@
 """Views for django_oauth2_lite application."""
 
 from django.shortcuts import render_to_response, get_object_or_404 
-from django_oauth2_lite.models import Client, Code, Scope, scope_by_name, code_by_token, token_by_value
-from django_oauth2_lite.forms import CodeForm, AccessRequest
+from django_oauth2_lite.models import Client, scope_by_name, code_by_token, token_by_value
+from django_oauth2_lite.forms import CodeForm, ClientForm
 from django.http import HttpResponseBadRequest, HttpResponse,\
     HttpResponseRedirect
 from django.utils import simplejson
@@ -23,9 +23,26 @@ def _post(request,key,dflt=None):
     else:
         return dflt
 
-@clientauth_required
-def authorize(request,template='templates/django_oauth2_lite/authorize.html'):
+@login_required
+def do_authorize(request,state,template_name):
+    client = get_object_or_404(Client,client_id=_get(request,'client_id'))
+        
+    if _get(request,'response_type','code') != 'code':
+        return client.redirect({'error': 'unsupported_response_type','state': state})
     
+    code = client.new_authz_code(owner=request.user,state=state)
+    for n in _get(request,'scope',"").split(' '):
+        scope = scope_by_name(n)
+        if scope == None:
+            return client.redirect({'error': 'invalid_scope','state': state})
+        code.scopes.add(scope)
+        
+    form = CodeForm(instance=code,state=state)
+
+    return render_to_response(template_name,{"form": form})
+
+@clientauth_required
+def authorize(request,template_name='django_oauth2_lite/authorize.html'):
     state = None
     if request.REQUEST.has_key('state'):
         state = request.REQUEST['state']
@@ -42,26 +59,14 @@ def authorize(request,template='templates/django_oauth2_lite/authorize.html'):
                 code.save()
                 return code.token.client.redirect({'state': state,'code': code.value})
             else:
+                code.token.delete()
                 code.delete()
                 return code.token.client.redirect({'error': 'access_denied','state': state})
         else:
             return code.token.client.redirect({'error': 'invalid_request','state': state})  
     else:
-        client = get_object_or_404(Client,client_id=_get(request,'client_id'))
-        
-        if _get(request,'response_type','code') != 'code':
-            return code.client.redirect({'error': 'unsupported_response_type','state': state})
-        
-        code = client.new_authz_code(state=state)
-        for n in _get(request,'scope',"").split(' '):
-            scope = scope_by_name(n)
-            if scope == None:
-                return client.redirect({'error': 'invalid_scope','state': state})
-            code.scopes.add(scope)
-            
-        form = CodeForm(instance=code,state=state)
-    
-    return render_to_response(template,{"form": form})
+        return do_authorize(request,state,template_name)
+
 
 def json_response(data):
     r = HttpResponse(simplejson.dumps(data),content_type='application/json')
@@ -101,22 +106,30 @@ def token(request):
                           'refresh_token': at.refresh_token.value})
     
 @login_required
-def clients(request,template_name='templates/django_oauth2_lite/clients.html'):
+def clients(request,template_name='django_oauth2_lite/clients.html'):
     queryset = Client.objects.filter(owner=request.user)
-    return object_list(request, 
+    return object_list(request,
+                       template_object_name='client',
                        queryset=queryset, 
                        template_name=template_name)
 
 @login_required
-def add_client(request,template_name="templates/django_oauth2_lite/client_form.html"):
-    return create_object(request, 
-                         model=Client, 
-                         template_name=template_name,
-                         post_save_redirect="/oauth/clients",
-                         extra_context={'user': request.user})
-
+def add_client(request,template_name="django_oauth2_lite/client_form.html"):
+    if request.method == 'POST':
+        client = Client(owner=request.user)
+        form = ClientForm(request.POST,request.FILES,instance=client)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("../clients")
+    else:
+        form = ClientForm()
+        
+    return render_to_response(template_name,{'form': form})
+    
 @login_required
-def remove_client(request,pk):
-    client = get_object_or_404(Client,id=pk)
+def remove_client(request,id):
+    client = get_object_or_404(Client,id=id)
     client.delete()
-    return HttpResponseRedirect("/oauth/clients")
+    return HttpResponseRedirect("../../clients")
+
+# Manage scopes in the admin view
