3187c2b06e32f9d6576b08d39e662e5b5822938f
Leif Johansson <leifj@sunet.se>
2011-11-03 17:27:02 +0100

    full set of urls

--- django_oauth2_lite/urls.py
+++ django_oauth2_lite/urls.py
@@ -1,8 +1,11 @@
 """URLs for django-oauth2-lite application."""
 
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns,url
 
-
-urlpatterns = patterns('django-oauth2-lite.views',
-    url(r'^$', view='index', name='django-oauth2-lite_index'),
+urlpatterns = patterns('django_oauth2_lite.views',
+    url(r'^token/?$', view='token'),
+    url(r'^authorize/?$', view='authorize'),
+    url(r'^clients?/?$', view='clients'),
+    url(r'^client/add/?$', view='add_client'),
+    url(r'^client/(?P<id>[0-9]+)/remove/?$', view='remove_client'),
 )
