87ed9b1ca1ddd7a320eadba58b2d088ca1bad64b
Leif Johansson <leifj@sunet.se>
2011-11-07 12:57:45 +0100

    add code token value to the CodeForm

--- django_oauth2_lite/forms.py
+++ django_oauth2_lite/forms.py
@@ -8,10 +8,13 @@ from django.forms.models import ModelForm
 from django_oauth2_lite.models import Code, Client
 from django.forms.forms import Form
 from django.forms.fields import CharField, URLField
+from django.forms.widgets import HiddenInput
     
 class CodeForm(ModelForm):
+    code = CharField(widget=HiddenInput)
     
     class Meta:
+        fields = ['authorized']
         model = Code
         
 class ClientForm(ModelForm):
